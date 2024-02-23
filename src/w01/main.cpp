2c2c00ca6271488b81b7827b29f1c4328e097a9a
THISISAGOODNAME <yangyj19931231@gmail.com>
2024-02-23 11:20:42 +0800

    w01 temp code

--- src/w01/main.cpp
+++ src/w01/main.cpp
@@ -5,6 +5,12 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <memory>
+#include <limits>
+#include <numbers>
+
+const float infinity = std::numeric_limits<float>::infinity();
+const float pi = std::numbers::pi_v<float>;
 
 class ray
 {
@@ -29,6 +35,99 @@ void push_color(std::vector<uint8_t>& image_data, HMM_Vec3 pixel_color)
     image_data.push_back(static_cast<int>(255.999 * pixel_color.Z));
 }
 
+struct hit_record
+{
+    HMM_Vec3 p;
+    HMM_Vec3 normal;
+    float t;
+    bool front_face;
+
+    void set_face_normal(const ray& r, const HMM_Vec3& outward_normal)
+    {
+        // Sets the hit record normal vector.
+        // **NOTE**: the parameter `outward_normal` is assumed to have unit length.
+
+        front_face = HMM_Dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class hittable
+{
+public:
+    virtual ~hittable() = default;
+    virtual bool hit(const ray& r, float ray_tmin, float ray_tmax, hit_record& rec) const = 0;
+};
+
+class sphere : public hittable
+{
+public:
+    sphere(HMM_Vec3 _center, float _radius) : center(_center), radius(_radius) {}
+
+    bool hit(const ray& r, float ray_tmin, float ray_tmax, hit_record& rec) const override
+    {
+        HMM_Vec3 oc = r.origin() - center;
+        auto a = HMM_LenSqr(r.direction());
+        auto half_b = HMM_Dot(oc, r.direction());
+        auto c = HMM_LenSqr(oc) - (radius * radius);
+
+        auto discriminant = half_b*half_b - a*c;
+        if (discriminant < 0) return false;
+        auto sqrtd = std::sqrtf(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        auto root = (-half_b - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root)
+        {
+            root = (-half_b + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        HMM_Vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+
+private:
+    HMM_Vec3 center;
+    float radius;
+};
+
+class hittable_list : public hittable
+{
+public:
+    std::vector<std::shared_ptr<hittable>> objects;
+
+    hittable_list() {}
+    hittable_list(std::shared_ptr<hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+    void add(std::shared_ptr<hittable> object) { objects.push_back(object); }
+
+    bool hit(const ray& r, float ray_tmin, float ray_tmax, hit_record& rec) const override
+    {
+        hit_record temp_rcc;
+        bool hit_anything = false;
+        float closest_so_far = ray_tmax;
+
+        for (const auto& object : objects)
+        {
+            if (object->hit(r, ray_tmin, closest_so_far, temp_rcc))
+            {
+                hit_anything = true;
+                closest_so_far = temp_rcc.t;
+                rec = temp_rcc;
+            }
+        }
+
+        return hit_anything;
+    }
+};
+
 float hit_sphere(const HMM_Vec3& center, float radius, const ray& r)
 {
     HMM_Vec3 oc = r.origin() - center;
@@ -50,7 +149,6 @@ HMM_Vec3 ray_color(const ray& r)
     {
         HMM_Vec3 N = HMM_Norm(r.at(t) - HMM_Vec3{0, 0, -1});
         return 0.5f * (N + HMM_Vec3{1, 1, 1});
-//        return 0.5f * HMM_Vec3{N.X + 1, N.Y + 1, N.Z + 1};
     }
 
     HMM_Vec3 unit_direction = HMM_Norm(r.direction());
@@ -89,11 +187,10 @@ int main()
     HMM_Vec3 viewport_upper_left = camera_center - HMM_Vec3{0, 0, focal_length} - viewport_u/2 - viewport_v/2;
     HMM_Vec3 pixel00_loc = viewport_upper_left + 0.5f * (pixel_delta_u + pixel_delta_v);
 
-    for (int j = 0; j < image_height; ++j) {
-        for (int i = 0; i < image_width; ++i) {
-            if ( i == 320 && j == 180)
-                int a = 0;
-
+    for (int j = 0; j < image_height; ++j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
